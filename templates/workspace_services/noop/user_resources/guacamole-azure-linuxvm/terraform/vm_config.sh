e42f3128c12426d26d10f2e9183fa1c44bfa556a
Stuart Leeks <stuartle@microsoft.com>
2022-08-22 18:10:17 +0000

    explore minimal templates

---
+++ templates/workspace_services/noop/user_resources/guacamole-azure-linuxvm/terraform/vm_config.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+# set -o nounset
+# Uncomment this line to see each command for debugging (careful: this will show secrets!)
+# set -o xtrace
+
+# Remove apt sources not included in sources.list file
+sudo rm -f /etc/apt/sources.list.d/*
+
+# Update apt packages from configured Nexus sources
+sudo apt-get update
+
+# Install xrdp so Guacamole can connect via RDP
+sudo apt-get install xrdp -y
+sudo adduser xrdp ssl-cert
+
+# Install desktop environment if image doesn't have one already
+if [ "${INSTALL_UI}" -eq 1 ]; then
+  sudo apt-get install xorg xfce4 xfce4-goodies dbus-x11 x11-xserver-utils -y
+  echo xfce4-session > ~/.xsession
+fi
+
+# Fix for blank screen on DSVM (/sh -> /bash due to conflict with profile.d scripts)
+sudo sed -i 's|!/bin/sh|!/bin/bash|g' /etc/xrdp/startwm.sh
+
+# Make sure xrdp service starts up with the system
+sudo systemctl enable xrdp
+
+if [ "${SHARED_STORAGE_ACCESS}" -eq 1 ]; then
+  # Install required packages
+  sudo apt-get install autofs -y
+
+  # Pass in required variables
+  storageAccountName="${STORAGE_ACCOUNT_NAME}"
+  storageAccountKey="${STORAGE_ACCOUNT_KEY}"
+  httpEndpoint="${HTTP_ENDPOINT}"
+  fileShareName="${FILESHARE_NAME}"
+  mntRoot="/fileshares"
+  credentialRoot="/etc/smbcredentials"
+
+  mntPath="$mntRoot/$fileShareName"
+  # shellcheck disable=SC2308
+  smbPath=$(echo "$httpEndpoint" | cut -c7-"$(expr length "$httpEndpoint")")$fileShareName
+  smbCredentialFile="$credentialRoot/$storageAccountName.cred"
+
+  # Create required file paths
+  sudo mkdir -p "$mntPath"
+  sudo mkdir -p "/etc/smbcredentials"
+  sudo mkdir -p $mntRoot
+
+  ### Auto FS to persist storage
+  # Create credential file
+  if [ ! -f "$smbCredentialFile" ]; then
+      echo "username=$storageAccountName" | sudo tee "$smbCredentialFile" > /dev/null
+      echo "password=$storageAccountKey" | sudo tee -a "$smbCredentialFile" > /dev/null
+  else
+      echo "The credential file $smbCredentialFile already exists, and was not modified."
+  fi
+
+  # Change permissions on the credential file so only root can read or modify the password file.
+  sudo chmod 600 "$smbCredentialFile"
+
+  # Configure autofs
+  echo "$fileShareName -fstype=cifs,rw,dir_mode=0777,credentials=$smbCredentialFile :$smbPath" | sudo tee /etc/auto.fileshares > /dev/null
+  echo "$mntRoot /etc/auto.fileshares --timeout=60" | sudo tee /etc/auto.master > /dev/null
+
+  # Restart service to register changes
+  sudo systemctl restart autofs
+
+  # Autofs mounts when accessed for 60 seconds.  Folder created for constant visible mount
+  sudo ln -s "$mntPath" "/$fileShareName"
+fi
+
+### Anaconda Config
+if [ "${CONDA_CONFIG}" -eq 1 ]; then
+  export PATH="/anaconda/condabin":$PATH
+  export PATH="/anaconda/bin":$PATH
+  export PATH="/anaconda/envs/py38_default/bin":$PATH
+  conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda/  --system
+  conda config --add channels "${NEXUS_PROXY_URL}"/repository/conda-forge/  --system
+  conda config --remove channels defaults --system
+  conda config --set channel_alias "${NEXUS_PROXY_URL}"/repository/conda/  --system
+fi
+
+# Docker install and config
+sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin jq
+jq -n --arg proxy "${NEXUS_PROXY_URL}:8083" '{"registry-mirrors": [$proxy]}' > /etc/docker/daemon.json
+sudo systemctl daemon-reload
+sudo systemctl restart docker
---
+++ templates/workspace_services/noop/user_resources/guacamole-azure-windowsvm/terraform/.terraform.lock.hcl
@@ -0,0 +1,58 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.16.0"
+  constraints = "3.16.0"
+  hashes = [
+    "h1:cBZXnJ4WErrhAzoQ1IMUMkpRlUmr8KQ2a0vLKH6B2a8=",
+    "zh:02aecc67db3f7cf19bd39ed454824422c43a5dc9f18b44d9547bb79ba66e0beb",
+    "zh:1775b033e3a29395087d731387efc926251232c4469a6c262f7039669e2f3aed",
+    "zh:1b955c3134b8fa61486383fee609bc99e46883c9b5148cb8d3bdc3d6d25b1e5e",
+    "zh:1ca04c35917fcf9f15aa2f24ef52b823575efa213fcb6d241cd189fddb032268",
+    "zh:20663ca219acc95d1de2129aac941f08eca1093c61cd6775e9c0b239b70a573e",
+    "zh:28427df342789f106ce500a489c750d7971d67cb58c495274878dc55d52452fe",
+    "zh:2a2e0755b9ebedbb4dd55de53191ce02e0a5511648610bf816532cd1614f2d7a",
+    "zh:5cc4c086ff081379070ea8177025a92a53e3c7bec2eabbf8182efa146e05b371",
+    "zh:5df50ae712c2b6e850b5953d5b89a29aca98ef1ae5fac4cb9225080ac319207c",
+    "zh:944ec6ceac2a1af58b58c270db90992d5f32614714647f6086ebc42789fa0f15",
+    "zh:f1e2df2f7db13b234d2cfa5d7c70054df4039532829be6ce8ed11c6f99ba0cf5",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version = "3.3.2"
+  hashes = [
+    "h1:H5V+7iXol/EHB2+BUMzGlpIiCOdV74H8YjzCxnSAWcg=",
+    "zh:038293aebfede983e45ee55c328e3fde82ae2e5719c9bd233c324cfacc437f9c",
+    "zh:07eaeab03a723d83ac1cc218f3a59fceb7bbf301b38e89a26807d1c93c81cef8",
+    "zh:427611a4ce9d856b1c73bea986d841a969e4c2799c8ac7c18798d0cc42b78d32",
+    "zh:49718d2da653c06a70ba81fd055e2b99dfd52dcb86820a6aeea620df22cd3b30",
+    "zh:5574828d90b19ab762604c6306337e6cd430e65868e13ef6ddb4e25ddb9ad4c0",
+    "zh:7222e16f7833199dabf1bc5401c56d708ec052b2a5870988bc89ff85b68a5388",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:b1b2d7d934784d2aee98b0f8f07a8ccfc0410de63493ae2bf2222c165becf938",
+    "zh:b8f85b6a20bd264fcd0814866f415f0a368d1123cd7879c8ebbf905d370babc8",
+    "zh:c3813133acc02bbebddf046d9942e8ba5c35fc99191e3eb057957dafc2929912",
+    "zh:e7a41dbc919d1de800689a81c240c27eec6b9395564630764ebb323ea82ac8a9",
+    "zh:ee6d23208449a8eaa6c4f203e33f5176fa795b4b9ecf32903dffe6e2574732c2",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/template" {
+  version = "2.2.0"
+  hashes = [
+    "h1:94qn780bi1qjrbC3uQtjJh3Wkfwd5+tTtJHOb7KTg9w=",
+    "zh:01702196f0a0492ec07917db7aaa595843d8f171dc195f4c988d2ffca2a06386",
+    "zh:09aae3da826ba3d7df69efeb25d146a1de0d03e951d35019a0f80e4f58c89b53",
+    "zh:09ba83c0625b6fe0a954da6fbd0c355ac0b7f07f86c91a2a97849140fea49603",
+    "zh:0e3a6c8e16f17f19010accd0844187d524580d9fdb0731f675ffcf4afba03d16",
+    "zh:45f2c594b6f2f34ea663704cc72048b212fe7d16fb4cfd959365fa997228a776",
+    "zh:77ea3e5a0446784d77114b5e851c970a3dde1e08fa6de38210b8385d7605d451",
+    "zh:8a154388f3708e3df5a69122a23bdfaf760a523788a5081976b3d5616f7d30ae",
+    "zh:992843002f2db5a11e626b3fc23dc0c87ad3729b3b3cff08e32ffb3df97edbde",
+    "zh:ad906f4cebd3ec5e43d5cd6dc8f4c5c9cc3b33d2243c89c5fc18f97f7277b51d",
+    "zh:c979425ddb256511137ecd093e23283234da0154b7fa8b21c2687182d9aea8b2",
+  ]
+}
